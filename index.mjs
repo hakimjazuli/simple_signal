d35a3cc368b591e1e99021e5e721843fe34712db
hakim_jazuli <muhammadhakimjazuli@gmail.com>
2024-06-29 11:54:34 +0700

    pub ok

--- index.mjs
+++ index.mjs
@@ -1,84 +1,189 @@
-// @ts-ignore
+// @ts-check
 
-import { _QueueObjectFIFO, _QueueFIFO } from '@html_first/simple_queue';
+class Functions {
+	/**
+	 * timeout
+	 * @public
+	 * @param {number} ms
+	 */
+	static T = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+	/**
+	 * is_async
+	 * @public
+	 * @param {CallableFunction} callback
+	 */
+	static IA = (callback) => callback.constructor.name === 'AsyncFunction';
+}
 
-let subscriber = null;
-const queue_handler = new _QueueFIFO();
+class _QueueObjectFIFO {
+	/**
+	 * details
+	 * @type {[callback:()=>(any|Promise<any>),debounce:(number|false)]}
+	 */
+	D;
+	/**
+	 * @param {()=>(any|Promise<any>)} callback
+	 * @param {number|false} [debounce]
+	 * - in ms
+	 */
+	constructor(callback, debounce = false) {
+		this.D = [callback, debounce];
+	}
+}
+
+class _QueueFIFO {
+	/**
+	 * queue
+	 * @private
+	 * @type {_QueueObjectFIFO['D'][]}
+	 */
+	Q = [];
+	/**
+	 * is_running
+	 * @private
+	 * @type {boolean}
+	 */
+	IR = false;
+	/**
+	 * assign
+	 * @public
+	 * @param {_QueueObjectFIFO} _queue
+	 */
+	A = (_queue) => {
+		this.P(_queue);
+		if (!this.IR) {
+			this.R();
+		}
+	};
+	/**
+	 * push
+	 * @private
+	 * @param {_QueueObjectFIFO} _queue
+	 */
+	P = (_queue) => {
+		this.Q.push(_queue.D);
+	};
+	/**
+	 * run
+	 * @private
+	 */
+	R = async () => {
+		this.IR = true;
+		while (this.Q.length !== 0) {
+			for (let i = 0; i < this.Q.length; i++) {
+				const [callback, debounce_ms] = this.Q[i];
+				this.Q.shift();
+				if (debounce_ms) {
+					await Functions.T(debounce_ms);
+				}
+				if (Functions.IA(callback)) {
+					await callback();
+					break;
+				}
+				callback();
+				break;
+			}
+		}
+		this.IR = false;
+	};
+}
+const helper = new (class {
+	/**
+	 * @type {null|(()=>Promise<void>)}
+	 */
+	S = null;
+	QH;
+	D;
+	/**
+	 * Description
+	 * @param {number|false} debounce
+	 */
+	constructor(debounce = false) {
+		this.D = debounce;
+		this.QH = new _QueueFIFO();
+	}
+})();
 
 /**
  * @template V
- * @typedef {{
- *   get value(): V,
- *   set value(newValue: V)
- * }} let_type_returns
  */
-/**
- * @callback let_type
- * @param {V} value -
- * @returns {let_type_returns<V>}
- */
-/**
- * @type {let_type}
- */
-const signal = (value) => {
+export class Let {
 	/**
+	 * subscription
+	 * @private
 	 * @type {(()=>Promise<void>)[]}
 	 */
-	const subscription = [];
-	return {
-		get value() {
-			if (subscriber) {
-				subscription.push(subscriber);
-			}
-			return value;
-		},
-		set value(new_value) {
-			value = new_value;
-			queue_handler.assign(
-				new _QueueObjectFIFO(async () => {
-					await Promise.all(
-						subscription.map(async (callback) => {
-							try {
-								return await callback();
-							} catch (error) {
-								console.error('Error in callback:', error);
-								throw error;
-							}
-						})
-					).catch((error) => {
-						console.error('Promise.all failed:', error);
-					});
-				}, false)
-			);
-		},
-	};
-};
+	S = [];
+	/**
+	 * value placeholder
+	 * @private
+	 * @type {V}
+	 */
+	V_;
+	/**
+	 * @param {V} value
+	 */
+	constructor(value) {
+		this.V_ = value;
+	}
+	/**
+	 * @returns {V}
+	 */
+	get value() {
+		if (helper.S) {
+			this.S.push(helper.S);
+		}
+		return this.V_;
+	}
+	/**
+	 * @param {V} newValue
+	 */
+	set value(newValue) {
+		this.V_ = newValue;
+		if (!this.S) {
+			return;
+		}
+		helper.QH.A(
+			new _QueueObjectFIFO(async () => {
+				await Promise.all(
+					this.S.map(async (callback) => {
+						try {
+							return await callback();
+						} catch (error) {
+							console.error('Error in callback:', error);
+							throw error;
+						}
+					})
+				).catch((error) => {
+					console.error('Promise.all failed:', error);
+				});
+			}, false)
+		);
+	}
+}
+
+export class $ {
+	/**
+	 * @param {()=>Promise<void>} asyncCallback
+	 */
+	constructor(asyncCallback) {
+		helper.S = asyncCallback;
+		asyncCallback();
+		helper.S = null;
+	}
+}
 
 /**
- * @param {()=>Promise<void>} fn
- * @returns {Promise<void>}
+ * @template V
  */
-const $ = async (fn) => {
-	subscriber = fn;
-	await fn();
-	subscriber = null;
-};
-
-/**
- * @callback derived_type
- * @param {()=>Promise<void>} fn
- * @returns {let_type_returns}
- */
-/**
- * @type {derived_type}
- */
-const derived = (fn) => {
-	const computed_ = signal();
-	$(async () => {
-		computed_.value = await fn();
-	});
-	return computed_;
-};
-window['signal'] = signal;
-window['$'] = $;
-window['derived'] = derived;
+export class Derived extends Let {
+	/**
+	 * @param {()=>Promise<V>} asyncCallback
+	 */
+	constructor(asyncCallback) {
+		super('');
+		new $(async () => {
+			this.value = await asyncCallback();
+		});
+	}
+}
